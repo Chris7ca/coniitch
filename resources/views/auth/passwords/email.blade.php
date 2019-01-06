61b1586efc8d15fff0d8d4824c66b68c565078fd
Citpsi <sistemas.citpsi@uaem.mx>
2019-01-06 01:04:14 -0600

    Reseteo de contraseña

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -7,6 +7,13 @@
     <h3>Recupera tu contraseña</h3>
     <p>Ingresa tu correo electrónico para que podamos enviarte información sobre cómo acceder nuevamente a la plataforma</p>
 
+    @if (session('status'))
+        <div class="uk-alert-success uk-animation-fade uk-margin" uk-alert>
+            <a class="uk-alert-close" uk-close></a>
+            {{ session('status') }}
+        </div>
+    @endif
+
     <form method="POST" action="{{ route('password.email') }}" class="uk-form-stacked uk-margin-medium">
 
         @csrf
@@ -14,6 +21,11 @@
         <div class="uk-margin-medium">
             <label class="uk-form-label">Correo electrónico</label>
             <input type="email" class="uk-input" name="email" value="{{ old('email') }}" autofocus required placeholder="Campo requerido...">
+            @if ($errors->has('email'))
+                <span class="uk-text-danger uk-text-bold" role="alert">
+                    <small>{{ $errors->first('email') }}</small>
+                </span>
+            @endif
         </div>
 
         <div class="uk-margin-medium">
