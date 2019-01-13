f9316b55ebec4cb8b27981ac05dca8a0b0d6ed13
Citpsi <sistemas.citpsi@uaem.mx>
2019-01-12 23:24:07 -0600

    Integración de componentes

---
+++ resources/views/app/root/roles.blade.php
@@ -0,0 +1,40 @@
+@extends('layouts.app')
+
+@section('title', 'Gestión de roles')
+
+@section('hero', 'Roles')
+
+@section('content')
+    
+    <section class="uk-container">
+
+        <div class="uk-grid" uk-grid>
+
+            <div class="uk-width-2-3@m">
+
+                <div>
+                
+                    <h3>Gestión de roles</h3>
+                    <hr class="uk-divider-small">
+
+                    <roles></roles>
+
+                </div>
+
+            </div>
+
+            <div class="uk-width-1-3@m">
+
+                <role class="uk-card uk-card-body uk-card-default uk-box-shadow-small"></role>
+
+            </div>
+
+        </div>
+
+    </section>
+
+@endsection
+
+@push('js')
+    <script src="{{ asset('js/roles.js') }}" defer></script>
+@endpush
---
+++ resources/views/app/root/users.blade.php
@@ -0,0 +1,50 @@
+@extends('layouts.app')
+
+@section('title', 'Gestión de usuarios')
+
+@section('hero', 'Usuarios')
+
+@section('content')
+    
+    <section class="uk-container">
+
+        <div class="uk-grid" uk-grid>
+
+            <div class="uk-width-2-3@m">
+
+                <div>
+
+                    <h3>Gestión de usuarios</h3>
+                    <hr class="uk-divider-small">
+
+                    <users></users>   
+
+                </div>
+
+            </div>
+
+            <div class="uk-width-1-3@m">
+
+                <div>
+
+                    <users-statistics class="uk-card uk-card-default uk-card-body uk-box-shadow-small"></users-statistics>
+
+                    <user class="uk-card uk-card-default uk-card-body uk-box-shadow-small uk-margin"></user>
+        
+                </div>
+
+            </div>
+
+        </div>
+
+    </section>
+
+@endsection
+
+@section('modals')
+    <modal-profile></modal-profile>
+@endsection
+
+@push('js')
+    <script src="{{ asset('js/users.js') }}" defer></script>
+@endpush
