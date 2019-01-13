350f08badb36e51708a7ffce311b89e40c703ee7
Citpsi <sistemas.citpsi@uaem.mx>
2019-01-12 23:07:05 -0600

    Factory de usuarios

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -13,11 +13,19 @@ use Faker\Generator as Faker;
 |
 */
 
-$factory->define(App\User::class, function (Faker $faker) {
+$factory->define(App\Models\User::class, function (Faker $faker) {
+
+    $methods = ['Traditional','Facebook','Google'];
+
     return [
-        'name' => $faker->name,
+        'first_name' => $faker->name,
+        'last_name' => $faker->lastName,
+        'has_personal_profile' => 1,
+        'has_academic_profile' => 1,
+        'has_emergency_contact' => 1,
         'email' => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
+        'method_to_register' => $methods[array_rand($methods, 1)],
         'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
         'remember_token' => str_random(10),
     ];
