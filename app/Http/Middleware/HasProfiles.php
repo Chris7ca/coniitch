ed216cf75068ab81bff724a51a91576f2c2c57c4
Citpsi <sistemas.citpsi@uaem.mx>
2019-01-12 23:11:28 -0600

    Middleware HasProfiles

---
+++ app/Http/Middleware/HasProfiles.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class HasProfiles
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+
+        $exept_routes = [
+            'app.profiles.view',
+            'app.profiles.personal.edit',
+            'app.profiles.personal.update',
+            'app.profiles.academic.edit',
+            'app.profiles.academic.update',
+            'app.profiles.professional.edit',
+            'app.profiles.professional.update',
+            'app.emergency.contact.edit',
+            'app.emergency.contact.update',
+            'app.user.password.update'
+        ];
+
+        $has_profiles = (Auth()->user()->has_personal_profile 
+            && Auth()->user()->has_academic_profile 
+            && Auth()->user()->has_emergency_contact);
+
+        if (!in_array($request->route()->getName(), $exept_routes) && !$has_profiles) {
+            
+            return redirect('app/perfil/editar')
+                    ->with('doesntHaveProfiles', 'Por favor completa la siguiente información.');
+        }
+
+        return $next($request);
+    }
+}
