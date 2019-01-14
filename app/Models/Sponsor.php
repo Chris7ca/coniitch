880280cb85f785f82aac92316f8406a5d9a71da3
Citpsi <sistemas.citpsi@uaem.mx>
2019-01-13 23:25:05 -0600

    Modelo Patrocinadores

---
+++ app/Models/Sponsor.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use App\Traits\HashId;
+use Illuminate\Database\Eloquent\Model;
+
+class Sponsor extends Model
+{
+    
+    use HashId;
+
+    protected $guarded = ['id'];
+
+    protected $appends = ['public_id'];
+
+    protected $hidden = ['id'];
+
+}
