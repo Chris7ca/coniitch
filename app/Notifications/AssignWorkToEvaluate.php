cf3d43bdb0e137574a80c54fbc6f9c98c877be53
SistemasCitpsi <sistemas.citpsi@uaem.mx>
2019-02-11 17:16:51 -0600

    Notificación de una asignación de trabajo

---
+++ app/Notifications/AssignWorkToEvaluate.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+
+class AssignWorkToEvaluate extends Notification implements ShouldQueue
+{
+    use Queueable;
+
+    protected $work;
+
+    public function __construct($work)
+    {
+        $this->work = $work;
+    }
+    
+    public function via($notifiable)
+    {
+        return ['database'];
+    }
+    
+    public function toArray($notifiable)
+    {
+        return [
+            'icon' => 'search',
+            'title' => 'Te ha sido asignado un trabajo a evaluar',
+            'description' => 'El titulo del trabajo es "' . $this->work->title . '". Por favor responde los más pronto posible y recuerda que en caso de aceptar 
+            el trabajo a evaluar, cuentas con 5 días para hacerlo.',
+            'route' => route('app.revisors.works.view')
+        ];
+    }
+}
