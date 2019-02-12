324f0b72c6b2e2786764a06eaed5aee8d456ce7e
SistemasCitpsi <sistemas.citpsi@uaem.mx>
2019-02-12 08:52:32 -0600

    Límite de palabras

--- resources/js/components/helpers/Editor.vue
+++ resources/js/components/helpers/Editor.vue
@@ -1,14 +1,39 @@
 <template>
-    <div class="uk-textarea" :id="id" :value="value"></div>
+    <div>
+        <div class="uk-textarea" :id="id" :value="value"></div>
+        <span :class="{ 'uk-text-meta uk-align-right': true, 'uk-text-danger' : error.words }" v-if="countWords">
+            {{ words }} palabras de {{ limitWords }}
+        </span>
+    </div>
 </template>
 
 <script>
     
     export default {
-        props:['id','value'],
+        props:['id','value','countWords','limitWords'],
         data() {
             return {
-                editor: null
+                editor: null,
+                words: 0,
+                error: {
+                    words: false
+                }
+            }
+        },
+        watch: {
+            value: function () {
+                this.countWordsInText();
+            }
+        },
+        methods: {
+            countWordsInText: function () {
+
+                let plainText   = document.getElementById(this.id).innerText;
+                let arrayWords  = plainText.trim().replace(/\s+/gi, ' ').split(' ');
+
+                this.words       = (arrayWords[0] == "" && arrayWords.length == 1) ? 0 : arrayWords.length;
+                this.error.words = (this.words > this.limitWords);
+                this.$emit('limit-words', (this.words - this.limitWords));
             }
         },
         beforeUpdate () {
@@ -23,7 +48,7 @@
 
             const ckeditorId = this.id
 
-            BalloonEditor.create( document.querySelector( `#${ckeditorId}` ),{
+            BalloonEditor.create( document.querySelector( `#${ckeditorId}` ), {
                 heading: {
                     options: [
                         { model: 'paragraph', title: 'Párrafo', class: 'ck-heading_paragraph' },
@@ -33,12 +58,15 @@
                 }
             } )
             .then( editor => {
+
                 this.editor = editor;
                 this.editor.setData( this.value );
+                this.countWordsInText();
                 this.editor.model.document.on( 'change:data' , () => {
-                    if (this.editor.getData() !== this.value) {
+
+                    // if (this.editor.getData() !== this.value) {
                         this.$emit('input', this.editor.getData());
-                    }
+                    // }
                 } );
             } )
             .catch( err => {
