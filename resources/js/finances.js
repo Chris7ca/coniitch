35f6bdf84e3943a2a7aa0f69a78c0411d03a4a7b
SistemasCitpsi <sistemas.citpsi@uaem.mx>
2019-02-22 10:44:51 -0600

    Componentes para la gestión de finanzas

---
+++ resources/js/finances.js
@@ -0,0 +1,30 @@
+require('./bootstrap');
+
+import VueContentPlaceholders from 'vue-content-placeholders';
+
+Vue.use(VueContentPlaceholders);
+
+Vue.component('records',                require('./components/finances/Records.vue').default);
+Vue.component('modal-profile',          require('./components/users/ModalProfile.vue').default);
+
+Vue.component('services',               require('./components/finances/Services.vue').default);
+Vue.component('modal-service',          require('./components/finances/Service.vue').default);
+Vue.component('modal-discount',         require('./components/finances/Discount.vue').default);
+
+Vue.component('payments',               require('./components/finances/Payments.vue').default);
+Vue.component('payment-information',    require('./components/finances/PaymentInformation.vue').default);
+
+const app = new Vue({
+    el: '#app',
+    methods:{
+        logout: function() {
+            axios.post(route('logout'))
+            .then( response => {
+                window.location = route('site.login');
+            })
+            .catch( error => {
+                console.log(error);
+            });
+        }
+    }
+});
