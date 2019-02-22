4d5aa5857e676514cbd31811b073c1921196bc78
SistemasCitpsi <sistemas.citpsi@uaem.mx>
2019-02-21 19:41:12 -0600

    Rutas de todos los usuarios

---
+++ routes/users.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * Notificaciones
+ */
+
+Route::get('notificaciones',                                        'Users\NotificationsController@view')->name('notifications.view');
+Route::post('notificaciones',                                       'Users\NotificationsController@index')->name('notifications.index');
+Route::post('notificaciones/widget',                                'Users\NotificationsController@widget')->name('notifications.widget');
+Route::post('notificaciones/{uuid}/ver',                            'Users\NotificationsController@read')->name('notifications.read');
+
+/**
+ * Perfiles
+ */
+
+Route::get('perfiles/editar',                                       'Users\ProfilesController@view')->name('profile.edit');
+Route::post('perfiles/{id}/ver',                                    'Users\ProfilesController@show')->name('profiles.show');
+
+Route::post('perfiles/avatar/actualizar',                           'Users\PersonalProfileController@avatar')->name('avatar.update');
+
+Route::post('perfiles/personal/editar',                             'Users\PersonalProfileController@edit')->name('profile.personal.edit');
+Route::post('perfiles/personal/actualizar',                         'Users\PersonalProfileController@update')->name('profile.personal.update');
+
+/**
+ * Password
+ */
+
+Route::post('perfiles/contrasena/actualizar',                       'Users\UsersController@password')->name('password.update');
