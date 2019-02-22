760edf9fc4dba7c3fd87052d6fa7dfceaf213266
SistemasCitpsi <sistemas.citpsi@uaem.mx>
2019-02-22 10:10:29 -0600

    Vista de registros para finanzas

---
+++ resources/views/app/finances/records.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.app')
+
+@section('title', 'Registros')
+
+@section('hero', 'Registros')
+
+@section('content')
+
+    <section class="uk-container uk-margin">
+
+        <h3>Registros de cogresistas</h3>
+
+        <records></records>
+        
+    </section>
+
+@endsection
+
+@section('modals')
+    <modal-profile></modal-profile>
+@endsection
+
+@push('js')
+    <script src="{{ asset('js/finances.js') }}" defer></script>
+@endpush
