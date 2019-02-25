980accfb8180ea71d4508cd38f01715846552819
SistemasCitpsi <sistemas.citpsi@uaem.mx>
2019-02-25 17:10:47 -0600

    Eliminación de la tabla de resultados de busqueda

--- resources/js/components/root/Users.vue
+++ resources/js/components/root/Users.vue
@@ -23,7 +23,8 @@
 
         </nav>
 
-        <table class="uk-table uk-table-middle uk-table-divider uk-table-responsive table-cards" v-if="!searchTableActive">
+        <table class="uk-table uk-table-middle uk-table-divider uk-table-responsive table-cards">
+
             <thead>
                 <tr>
                     <th class="uk-table-shrink"></th>
@@ -33,6 +34,7 @@
                     <th class="uk-table-shrink">Acciones</th>
                 </tr>
             </thead>
+
             <tbody>
 
                 <tr v-if="!dataLoaded">
@@ -70,61 +72,25 @@
                             <li>
                                 <a role="link" @click="deleteUser(user.public_id)" uk-icon="trash"></a>
                             </li>
+                            <li>
+                                <a href="#modal-documents" @click="documents(user.public_id)" uk-icon="folder" uk-toggle></a>
+                            </li>
                         </ul>
                     </td>
                 </tr>
 
             </tbody>
+
+            <tfoot>
+                <tr v-if="dataLoaded && users.length == 0">
+                    <td class="uk-text-center" colspan="5" style="border: none;">
+                        <h5>No hemos encontrado usuarios ...</h5>
+                    </td>
+                </tr>
+            </tfoot>
+
         </table>
-
-        <table class="uk-table uk-table-middle uk-table-divider uk-table-responsive" v-if="searchTableActive">
-            <thead>
-                <tr>
-                    <th class="uk-table-shrink"></th>
-                    <th class="uk-width-small">Nombre</th>
-                    <th class="uk-width-small">Correo</th>
-                    <th>Roles</th>
-                    <th class="uk-table-shrink">Acciones</th>
-                </tr>
-            </thead>
-            <tbody>
-
-                <tr v-if="foundUsers.length == 0">
-                    <td colspan="5">
-                        <h5 class="uk-text-center uk-text-muted uk-margin-medium-top">No se econtraron usuarios</h5>
-                    </td>
-                </tr>
-
-                <tr v-for="(user, index) in foundUsers" :key="index" v-else>
-                    <td class="uk-text-center">
-                        <img :src="getAvatar(user.avatar)" class="avatar-small uk-preserve-width">
-                    </td>
-                    <td>
-                        <a href="#modal-profile" class="uk-button button-text uk-padding-remove" @click="showProfile(user.public_id)" uk-toggle>{{ fullName(user) }}</a>
-                    </td>
-                    <td>
-                        {{ user.email }}
-                    </td>
-                    <td>
-                        <small v-for="(role, i) in user.roles" class="uk-text-primary" :key="i"> {{ role.display_name }}.</small>
-                    </td>
-                    <td>
-                        <ul class="uk-iconnav uk-flex uk-flex-center">
-                            <li v-if="user.email_verified_at == null">
-                                <span class="uk-text-warning" uk-icon="warning" uk-tooltip="Usuario no verificado"></span>
-                            </li>
-                            <li>
-                                <a role="button" @click="editUser(user.public_id)" uk-icon="file-edit"></a>
-                            </li>
-                            <li>
-                                <a role="link" @click="deleteUser(user.public_id)" uk-icon="trash"></a>
-                            </li>
-                        </ul>
-                    </td>
-                </tr>
-                
-            </tbody>
-        </table>
+        
     </div>
 </template>
 
@@ -141,7 +107,7 @@
             return {
                 dataLoaded: false,
                 users: [],
-                foundUsers: [],
+                tempUsers: [],
                 searchTableActive: false,
                 querySearch: ''
             }
@@ -155,19 +121,20 @@
                     
                     axios.post(route('app.root.users.search', { search : val }))
                     .then( users => {
-                        this.foundUsers = users.data;
-                        this.searchTableActive = true;
-                        this.dataLoaded = true;
+                        this.users              = users.data;
+                        this.searchTableActive  = true;
+                        this.dataLoaded         = true;
                     })
                     .catch( error => {
-                        this.dataLoaded = true;
+                        this.dataLoaded        = true;
                         this.searchTableActive = false;
                         showAxiosErrorMessage(error);
                     });
 
-                } else {
-                    this.foundUsers = [];
-                    this.searchTableActive = false;
+                } else if ( val.length < 2 ) {
+                    console.log('ok')
+                    this.users              = this.tempUsers;
+                    this.searchTableActive  = false;
                 }
             }
         },
@@ -180,7 +147,8 @@
                 return user.first_name + ' ' + secondName + ' ' + user.last_name + ' ' + secondLastName;
             },
             updateItems: function (users) {
-                this.users = users;
+                this.users      = users;
+                this.tempUsers  = users;
             },
             updateLoader: function (status) {
                 this.dataLoaded = !status;
@@ -197,45 +165,36 @@
             deleteUser: function (id) {
 
                 let self = this;
-                let url = route('app.root.users.delete', { id : id });
+                let url  = route('app.root.users.delete', { id : id });
 
                 UIkit.modal.confirm('¿Estás seguro que deseas eliminar este usuario?').then(function () {
                     axios.delete(url)
                     .then( response => {
-                        if (self.searchTableActive) {
-                            let index = self.foundUsers.findIndex( u => u.public_id == id );
-                            self.foundUsers.splice(index, 1);
-                        } else {
-                            let index = self.users.findIndex( u => u.public_id == id );
-                            self.users.splice(index, 1);
-                        }
+
+                        let index = self.users.findIndex( u => u.public_id == id );
+
+                        self.users.splice(index, 1);
                         UIkit.notification(response.data.message, 'success');
                     })
                     .catch( error => {
                         showAxiosErrorMessage(error);
                     });
-                }, null);
+                }, () => {});
+            },
+            documents: function (id) {
+                EventBus.$emit('documentsUser', id);
             }
         },
         created() {
 
             EventBus.$on('userUpdated', (user) => {
 
-                let index = (this.searchTableActive) ?
-                    this.foundUsers.findIndex( u => u.public_id == user.public_id) :
-                    this.users.findIndex( u => u.public_id == user.public_id);
+                let index = this.users.findIndex( u => u.public_id == user.public_id);
 
-                if ( this.searchTableActive ){
-                    this.foundUsers[index].first_name = user.first_name;
-                    this.foundUsers[index].last_name = user.last_name;
-                    this.foundUsers[index].email = user.email;
-                    this.foundUsers[index].roles = user.roles;
-                } else {
-                    this.users[index].first_name = user.first_name;
-                    this.users[index].last_name = user.last_name;
-                    this.users[index].email = user.email;
-                    this.users[index].roles = user.roles;
-                }
+                this.users[index].first_name = user.first_name;
+                this.users[index].last_name = user.last_name;
+                this.users[index].email = user.email;
+                this.users[index].roles = user.roles;
             });
 
         }
