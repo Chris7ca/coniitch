1651f888feea239458f424ae5df3b355fbe5eca5
Citpsi <sistemas.citpsi@uaem.mx>
2019-03-04 12:12:12 -0600

    Presentación de servicios

--- resources/js/components/finances/Services.vue
+++ resources/js/components/finances/Services.vue
@@ -4,7 +4,7 @@
 
         <div class="uk-margin-medium">
             <a href="#modal-service" class="uk-link-reset" uk-toggle>
-                <span class="uk-margin-small-left" uk-icon="plus-circle"></span> Nuevo servicio
+                <span class="uk-margin-small-right" uk-icon="plus-circle"></span> Nuevo servicio
             </a>
         </div>
     
@@ -14,71 +14,76 @@
                 <tr>
                     <th>Servicio</th>
                     <th class="uk-table-expand">Detalles</th>
-                    <th class="uk-table-shink">Precio U.</th>
-                    <th class="uk-table-shink">Dirigido a</th>
+                    <th>Precio U.</th>
+                    <th>Dirigido a</th>
                     <th class="uk-text-center">Opciones</th>
                 </tr>
             </thead>
 
             <tbody>
 
-                <tr v-for="(service, index) in services" :key="index">
+                <tr v-for="service in services" :key="service.public_id">
                     
                     <td>
-                        {{ service.concept }}
+                        <h6>{{ service.concept }}</h6>
                     </td>
                     <td>
                         {{ service.details }}
                     </td>
                     <td>
-                        $ {{ service.price }} MXN
+                        $ {{ parseFloat(service.price).toFixed(2) }} MXN
                     </td>
                     <td>
-                        <small class="uk-label uk-margin-small-left" v-for="(user, i) in service.for_users" :key="i">
-                            {{ user.display_name }}
-                        </small> 
+                        <span class="uk-label uk-small" v-for="role in service.for" :key="role.public_id">{{ role.display_name }}</span>
                     </td>
                     <td>
                         <ul class="uk-iconnav uk-flex-center">
                             
                             <li>
-                                <a href="#modal-service" uk-icon="file-edit" @click="updateService(service)" uk-tooltip="Editar servicio" uk-toggle></a>
+                                <a href="#modal-service" uk-icon="file-edit" @click="editService(service)" uk-tooltip="Editar servicio" uk-toggle></a>
                             </li>
                             <li>
                                 <a role="button" uk-icon="trash" @click="deleteService(service)" uk-tooltip="Eliminar servicio"></a>
                             </li>
                             <li>
-                                <a role="button" uk-icon="tag" uk-tooltip="Descuento"></a>
+                                <a role="button" uk-icon="tag" uk-tooltip="Descuentos">
+                                    <sup v-if="service.discounts.length > 0">
+                                        <span class="uk-badge">{{ service.discounts.length }}</span>
+                                    </sup>
+                                </a>
 
                                 <div uk-dropdown="mode: click">
                                     
-                                    <div v-if="!service.discount">
-                                        <small>
-                                            No hay promociones o descuetos asociados a este servicio
-                                        </small>
+                                    <div>
+                                        <div v-if="service.discounts.length == 0">
+                                            <small>
+                                                No hay promociones o descuetos asociados a este servicio
+                                            </small>
+                                        </div>
+
+                                        <ul class="uk-list uk-list-divider" v-else>
+                                            <li v-for="discount in service.discounts" :key="discount.public_id">
+
+                                                <h6 class="uk-margin-remove">{{ discount.name }}</h6>
+                                                <span class="uk-text-meta">$ {{ parseFloat(discount.discount).toFixed(2) }} MX</span>
+
+                                                <ul class="uk-iconnav uk-padding-remove">
+                                                    <li>
+                                                        <a href="#modal-discount" uk-icon="icon: file-edit; ratio: 0.9;" @click="editDiscount(service, discount)" uk-toggle></a>
+                                                    </li>
+                                                    <li>
+                                                        <a role="button" uk-icon="icon: trash; ratio: 0.9;" @click="deleteDiscount(service, discount)"></a>
+                                                    </li>
+                                                </ul>
+
+                                            </li>
+                                        </ul>                                        
                                         
                                         <hr class="uk-divider-small">
 
                                         <a href="#modal-discount" class="uk-link-reset uk-text-small" @click="createDiscount(service)" uk-toggle>
                                             <span class="uk-margin-small-left" uk-icon="plus-circle"></span> Agregar descuento
                                         </a>
-                                    </div>
-                                    
-                                    <div v-else>
-                                        <p class="uk-text-bold">
-                                            {{ service.discount.name }}
-                                        </p>
-
-                                        <hr class="uk-divider-small">
-
-                                        <ul class="uk-iconnav">
-                                            <li>
-                                                <a href="#modal-discount" uk-icon="file-edit" @click="editDiscount(service)" uk-toggle></a>
-                                            </li>
-                                            <li>
-                                                <a role="button" uk-icon="trash" @click="deleteDiscount(service)"></a>
-                                            </li>
-                                        </ul>
                                     </div>
 
                                 </div>
@@ -125,14 +130,14 @@
             }
         },
         methods: {
-            updateService: function (service) {
+            editService: function (service) {
                 EventBus.$emit('editService', service);
             },
             createDiscount: function (service) {
                 EventBus.$emit('createDiscount', service);
             },  
-            editDiscount: function (service) {
-                EventBus.$emit('editDiscount', service);
+            editDiscount: function (service, discount) {
+                EventBus.$emit('editDiscount', service, discount);
             },
             deleteService: function (service) {
 
@@ -156,15 +161,18 @@
                 }, () => {});
 
             },
-            deleteDiscount: function (service) {
+            deleteDiscount: function (service, discount) {
                 UIkit.modal.confirm('¿Estás seguro que deseas elimiar este servicio? Ten en cuenta que puede haber pagos relacionados.').then(() => {
 
-                    let url = route('app.finances.services.discount.delete', { id : service.public_id, ID : service.discount.public_id});
+                    let url = route('app.finances.services.discount.delete', { id : service.public_id, ID : discount.public_id});
 
                     axios.delete(url)
                     .then( response => {
 
-                        service.discount = null;                        
+                        let indexService  = this.services.findIndex( s => s.public_id == service.public_id );
+                        let indexDiscount = this.services[indexService].discounts.findIndex( d => d.public_id == discount.public_id );
+                        
+                        this.services[indexService].discounts.splice(indexDiscount, 1);
                         UIkit.notification(response.data.message, 'success');
 
                     })
@@ -197,7 +205,7 @@
 
                 let index = this.services.findIndex( s => s.public_id == id );
 
-                this.services[index].discount = discount;
+                this.services[index].discounts.push(discount);
 
             });
 
