632fd9aff1824b155d19a3e930e9f7943cb53357
Citpsi <sistemas.citpsi@uaem.mx>
2019-03-04 10:32:36 -0600

    Modelo imagenes de servicios

--- database/migrations/2019_02_26_163823_create_service_images_table.php
+++ database/migrations/2019_02_26_163823_create_service_images_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateAssignedServicesTable extends Migration
+class CreateServiceImagesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,9 +13,10 @@ class CreateAssignedServicesTable extends Migration
      */
     public function up()
     {
-        Schema::create('assigned_services', function (Blueprint $table) {
+        Schema::create('service_images', function (Blueprint $table) {
+            $table->increments('id');
             $table->integer('service_id')->unsigned();
-            $table->integer('role_id')->unsigned();
+            $table->string('file');
         });
     }
 
@@ -26,6 +27,6 @@ class CreateAssignedServicesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('assigned_services');
+        Schema::dropIfExists('service_images');
     }
 }
